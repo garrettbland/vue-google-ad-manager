77bba355d1b718ee8a2abc711986010fa24ff38e
Michael Hatch <mhatch@sittercity.com>
2020-08-18 09:52:39 -0500

    [ch51902]: add key-value targeting

--- src/Ad.vue
+++ src/Ad.vue
@@ -16,6 +16,9 @@
             size: {},
             slotType: {
                 default: 'normal'
+            },
+            targetings: {
+                default: () => {[]}
             }
         },
 
@@ -62,6 +65,9 @@
             },
             defineSlot(){
                 googletag.defineSlot(this.adUnit, this.sizes, this.id).defineSizeMapping(this.mapping).addService(googletag.pubads());
+                this.targetings.forEach(elm => {
+                    googletag.pubads().setTargeting(elm.key, elm.value);
+                });
             },
             defineOutOfPageSlot(){
                 googletag.defineOutOfPageSlot(this.adUnit, this.id).addService(googletag.pubads())
@@ -73,7 +79,8 @@
         },
         beforeDestroy () {
             googletag.cmd.push(() => {
-                googletag.destroySlots()
+                googletag.destroySlots();
+                googletag.pubads().clearTargeting();
             })
         }
     }
